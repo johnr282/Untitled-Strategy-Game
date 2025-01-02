5687eea9758c77f6e7e159205a8020097a55b119
johnrose <johnrose@umich.edu>
2025-01-02 01:50:51 -0500

    Removed unused code from old state management system

--- Assets/Scripts/Utilities/ServerEvents.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// ------------------------------------------------------------------
-// Contains event definitions used with EventBus system that are
-// published on the server
-// ------------------------------------------------------------------ 
